832cf494bec9fd4bc1801107aaae84faaf5f7ee4
godlovetikum <godlovetikum@gmail.com>
2026-07-15 09:08:02 +0100

    Remove unused dependencies and clean up Android build configuration

--- android/app/src/main/java/com/forgelink/MainApplication.kt
+++ android/app/src/main/java/com/forgelink/MainApplication.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
@@ -23,6 +22,5 @@ class MainApplication : Application(), ReactApplication {
     override fun onCreate() {
         super.onCreate()
         SoLoader.init(this, OpenSourceMergedSoMapping)
-        load()
     }
 }
