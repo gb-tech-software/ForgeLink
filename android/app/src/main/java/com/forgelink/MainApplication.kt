b5986e9ed7884bd300d09823018642e03942d52e
godlovetikum <godlovetikum@gmail.com>
2026-07-15 11:40:17 +0100

    Add Firebase and update Android build configuration with library dependencies

--- android/app/src/main/java/com/forgelink/MainApplication.kt
+++ android/app/src/main/java/com/forgelink/MainApplication.kt
@@ -1,30 +1,23 @@
 package com.forgelink
 
 import android.app.Application
+import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage
-import com.reactnativecommunity.webview.RNCWebViewPackage
-import com.swmansion.gesturehandler.RNGestureHandlerPackage
-import com.swmansion.reanimated.ReanimatedPackage
 
 class MainApplication : Application(), ReactApplication {
     override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-        override fun getPackages(): List<ReactPackage> {
-            return listOf(
-                ForgeLinkPackage(),
-                ReanimatedPackage(),
-                RNGestureHandlerPackage(),
-                RNCWebViewPackage(),
-                AsyncStoragePackage(),
-            )
-        }
+        override fun getPackages(): List<ReactPackage> =
+            PackageList(this).packages.apply {
+                // ForgeLinkPackage is not an npm module, so autolinking won't find it
+                add(ForgeLinkPackage())
+            }
 
         override fun getJSMainModuleName(): String = "index"
     }
